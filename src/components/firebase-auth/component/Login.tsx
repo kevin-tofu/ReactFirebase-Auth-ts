0f9352e728daa93ca3745413480735a62ee3c26a
kohei <koheitech001@gmail.com>
2023-01-22 19:19:08 +0900

    change style with bs

--- src/components/firebase-auth/component/Login.tsx
+++ src/components/firebase-auth/component/Login.tsx
@@ -1,5 +1,6 @@
 import React, { useRef, useState } from 'react'
-import { Form, Button, Card, Alert } from 'react-bootstrap'
+import { Form, Button, Card, Alert, Stack } from 'react-bootstrap'
+
 import { useAuth } from '../context/AuthContext'
 import { Link, useNavigate } from 'react-router-dom'
 import 'bootstrap/dist/css/bootstrap.min.css';
@@ -37,22 +38,26 @@ function Login() {
         <Card.Body>
           <h2 className='text-center mb-4'>Log In</h2>
           {error && <Alert variant='danger'>{error}</Alert>}
-          <Form onSubmit={handleSubmit}>
-            <Form.Group id='email'>
-              <Form.Label>Email</Form.Label>
-              <Form.Control type='email' ref={emailRef} required />
-            </Form.Group>
-            <Form.Group id='password'>
-              <Form.Label>Password</Form.Label>
-              <Form.Control type='password' ref={passwordRef} required />
-            </Form.Group>
-            <Button variant="primary" disabled={loading} className='w-100' type='submit'>
-              Log In
-            </Button>
-          </Form>
-          <div className='w-100 text-center mt-3'>
-            <Link to='/forgot-password'>Forgot Password?</Link>
-          </div>
+          <Stack gap={2} className="row-md-5 mx-auto">
+            <Form onSubmit={handleSubmit}>
+              <Stack gap={2} className="row-md-5 mx-auto">
+                <Form.Group id='email'>
+                  <Form.Label>Email</Form.Label>
+                  <Form.Control type='email' ref={emailRef} required />
+                </Form.Group>
+                <Form.Group id='password'>
+                  <Form.Label>Password</Form.Label>
+                  <Form.Control type='password' ref={passwordRef} required />
+                </Form.Group>
+                <Button variant="primary" disabled={loading} className='w-100' type='submit'>
+                  Log In
+                </Button>
+              </Stack>
+            </Form>
+            <div className='w-100 text-center mt-3'>
+              <Link to='/forgot-password'>Forgot Password?</Link>
+            </div>
+          </Stack>
         </Card.Body>
       </Card>
       <div className='w-100 text-center mt-2'>
